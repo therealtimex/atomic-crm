695fdf9bcb524d5305e640306574a72607af703e
Trung Le <154565560+therealtimex@users.noreply.github.com>
2025-12-19 15:43:00 -0800

    chore: release v0.8.0

--- src/components/supabase/layout.tsx
+++ src/components/supabase/layout.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "react-router";
 import { Notification } from "@/components/admin/notification";
 import { useConfigurationContext } from "@/components/atomic-crm/root/ConfigurationContext";
 
@@ -10,10 +11,13 @@ export const Layout = ({ children }: React.PropsWithChildren) => {
       <div className="container relative grid flex-col items-center justify-center sm:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
+          <Link
+            to="/"
+            className="relative z-20 flex items-center text-lg font-medium no-underline text-white hover:opacity-80 transition-opacity"
+          >
             <img className="h-6 mr-2" src={darkModeLogo} alt={title} />
             {title}
-          </div>
+          </Link>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
